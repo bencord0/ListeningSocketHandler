bacab39f2c5ca8b1b723e3b9a9ac0c8adadd43d7
Ben Cordero <bmc@linux.com>
2012-12-13 00:49:17 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version='0.0.1',
     author='Ben Cordero',
     author_email='bmc@linux.com',
-    packages=['listeningsockethandler'],
     url='https://github.com/bencord0/ListeningSocketHandler',
     license='LICENSE.txt',
     description='The opposite of logging.handlers.SocketHandler',
