ad6e375c4a3f8ce7ef9705f37a546383ec5dfbb6
Ben Cordero <bmc@linux.com>
2012-12-14 22:21:49 +0000

    Architectural changes

--- ListeningSocketHandler/__init__.py
+++ ListeningSocketHandler/__init__.py
@@ -57,25 +57,3 @@ class ListeningSocketHandler(logging.Handler):
     def getsockname(self):
         return self._acceptor.getsockname()
 
-class TestListeningSocketHandler(unittest.TestCase):
-    def setUp(self):
-        self.log = logging.getLogger()
-        # default will bind to any unsed port.
-        self.lsh = ListeningSocketHandler()
-        self.log.addHandler(self.lsh)
-
-    def test_send_message(self):
-        self.log.warn("Sending a warning")
-
-    def test_getsockport(self):
-        self.assertIsInstance(self.lsh.getsockname()[1], int)
-
-    def test_recieve_message(self):
-        client = socket.socket()
-        client.connect(("localhost", self.lsh.getsockname()[1]))
-        self.log.warn("Sending a warning")
-        buf = client.recv(4096)
-        self.assertEqual(buf, "Sending a warning\n")
-
-if __name__ == '__main__':
-    unittest.main()
--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
-from distutils.core import setup
+import os
+from setuptools import setup
+from pkg_resources import parse_requirements
+
+req_fn = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+with open(req_fn) as f:
+    requirements = f.read()
 
 setup(
     name='ListeningSocketHandler',
-    version='0.0.1',
+    version='0.0.2',
     author='Ben Cordero',
     author_email='bmc@linux.com',
+    packages=['ListeningSocketHandler', 'ListeningSocketHandler.test'],
+    scripts=['bin/log-spewer.py', 'bin/log-reader.py'],
     url='https://github.com/bencord0/ListeningSocketHandler',
     license='LICENSE.txt',
     description='The opposite of logging.handlers.SocketHandler',
     long_description=open('README.txt').read(),
-    py_modules=['listeningsockethandler'],
+    install_requires=parse_requirements(requirements),
 )
